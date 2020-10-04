2ff9e0a01ee0291e3e8412429d74f9bb00a1c73f
josemip98 <j_miguel98@hotmail.com>
2020-10-04 23:52:42 +0200

    Añado clase main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-<html>
-  <head>
-    <title>Mi primer codigo JavaScript</title>
-  </head>
-  <body>
-    <h1>Mi primer código JavaScript</h1>
-    <script>
-      document.write("Hola Mundo");
-    </script>
-  </body>
-</html>
+	<script>
+		document.write("Hola Mundo");
+    	</script>
