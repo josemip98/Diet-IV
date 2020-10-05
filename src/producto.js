6b8602278d70a2650d824fd7a4e7427e9e742fa8
josemip98 <j_miguel98@hotmail.com>
2020-10-05 19:45:51 +0200

    Creada clase producto close #7

---
+++ src/producto.js
@@ -0,0 +1,16 @@
+
+class Producto {
+    //constructor
+    constructor(){
+        var producto;
+        var calorias;
+    }
+
+    mostrarProducto(){
+        return producto;
+    }
+
+    mostrarCalorias(){
+        return calorias;
+    }
+}
