b971de73214d7f8ea999992e689b209dad41ebf3
josemip98 <j_miguel98@hotmail.com>
2020-10-05 20:16:03 +0200

    Actualizada clase producto

--- src/producto.js
+++ src/producto.js
@@ -3,14 +3,34 @@ class Producto {
     //constructor
     constructor(){
         var producto;
+	var marca;
         var calorias;
+	var grasa;
+	var proteinas;
+	var hidratos;
     }
 
     mostrarProducto(){
         return producto;
     }
 
+    mostrarMarca(){
+        return marca;
+    }
+
     mostrarCalorias(){
         return calorias;
+    }
+
+    mostrarGrasa(){
+        return grasa;
+    }
+
+    mostrarProteinas(){
+        return proteinas;
+    }
+
+    mostrarHidratos(){
+        return hidratos;
     }
 }
