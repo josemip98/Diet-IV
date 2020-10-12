2bcafeeb0de7ada6fa61f551833e7b6f2de22ebf
Jose Miguel Pelegrina <37540705+josemip98@users.noreply.github.com>
2020-10-12 21:47:09 +0200

    Update dieta.js

--- src/dieta.js
+++ src/dieta.js
@@ -1,1 +1,25 @@
 
+class Dieta{
+
+  constructor(listaProductos){
+    listaProductos = new Array();
+  }
+  
+  AniadirProducto(producto){
+    if( producto == true){
+      throw new Error('Error inserción producto');
+    }
+    else{
+      this.listaProductos.push(producto);
+    }
+  }
+  
+  MostrarDieta(){
+    for(var i in this.listaProductos){
+      console.log(this.listaProductos[i]);
+    }
+  }
+  
+}
+
+module.exports = Dieta;
