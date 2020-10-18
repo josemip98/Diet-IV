2f30178f544a5d3d1631e61fed204a902bebcdf8
Jose Miguel Pelegrina <37540705+josemip98@users.noreply.github.com>
2020-10-18 20:02:01 +0200

    Creado archivo gulpfile.js close #25

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+var jest = require('gulp-jest').default;
+const shell = require('gulp-shell');
+
+// Tarea para instalación de dependencias
+gulp.task('install', shell.task(['npm install']));
+
+// Tarea para ejecución de tests
+gulp.task('test', () => (
+    gulp.src('tests', {read: false})
+        .pipe(jest({reporter:'default'}))
+));
