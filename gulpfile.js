ca1697f0b12d1e8b98978f9cb633fe2fe055f285
josemip98 <j_miguel98@hotmail.com>
2020-10-28 14:26:07 +0100

    arreglo gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 const gulp = require('gulp');
 var jest = require('gulp-jest').default;
-const shell = require('gulp-shell');
-
-// Tarea para instalación de dependencias
-gulp.task('install', shell.task(['npm install']));
 
 // Tarea para ejecución de tests
 gulp.task('test', () => (
