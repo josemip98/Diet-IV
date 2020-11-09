f60edd74e89dbc480bc167952ce7525bb53fc125
josemip98 <j_miguel98@hotmail.com>
2020-11-09 14:44:24 +0100

    Modifico gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
+const shell = require('gulp-shell');
 var jest = require('gulp-jest').default;
+
+// Tarea para instalación de dependencias
+gulp.task('install', shell.task(['npm install']));
 
 // Tarea para ejecución de tests
 gulp.task('test', () => (
