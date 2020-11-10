0f0222d9b2e1223146ebcf3dedced758c0b52c2e
josemip98 <j_miguel98@hotmail.com>
2020-11-10 12:42:54 +0100

    Modifico gulpfile.js [ci skip]

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const shell = require('gulp-shell');
 var jest = require('gulp-jest').default;
 
+// Tarea para instalación de dependencias
+gulp.task('install', shell.task(['npm install']));
+
 // Tarea para ejecución de tests
 gulp.task('test', () => (
     gulp.src('tests', {read: false})
