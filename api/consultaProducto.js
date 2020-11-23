4420fada50f678ce8d5105edd8b4dc7285931e35
josemip98 <j_miguel98@hotmail.com>
2020-11-23 14:52:20 +0100

    Añado funcion vercel [skip ci]

--- api/consultaProducto.js
+++ api/consultaProducto.js
@@ -7,7 +7,7 @@ module.exports = (req,res) =>{
     //Captaremos de la URL el producto del que queremos consultar la informacion nutricional
     //Si no obtenemos ningun por defecto valor "Vacio" para generar el resultado correspondiente
 
-    const{producto="Vacio"} = req.query
+    const{nombre="Vacio"} = req.query
     var result;
     var catalogo = new Catalogo();
     var i = 0;
@@ -16,9 +16,9 @@ module.exports = (req,res) =>{
     while( i < datos.datos.length){
         nombre= datos.datos[i]['Nombre'];
         calorias= datos.datos[i]["calorias"];
-  	grasa= datos.datos[i]["grasa"];
-	proteinas= datos.datos[i]["hidratos"];
-	hidratos= datos.datos[i]["proteinas"];
+  	    grasa= datos.datos[i]["grasa"];
+	      proteinas= datos.datos[i]["hidratos"];
+	      hidratos= datos.datos[i]["proteinas"];
         i+=1
         var producto = new Producto(nombre,calorias,grasa,proteinas,hidratos)
         catalogo.aniadirProducto(producto)
@@ -30,17 +30,17 @@ module.exports = (req,res) =>{
 
     if(producto == "arroz" || producto=="macarrones" || producto=="atun"){
         var productosNombre = Dieta.mostrarProducto(producto)
-        
+
         productosNombre.forEach(element => {
             datosJSON.push({
                 element.getProducto()
             });
-            
+
         });
         // FORMAMOS EL JSON
         objetoJSON.productos = datosJSON;
         result = JSON.stringify(objetoJSON)
-        
+
     }
     //Si es Vacio significa que no se ha recibido nada
     else if(genero == "Vacio"){
