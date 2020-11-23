43426cf7aea5819984002333d0fa5519e944332e
josemip98 <j_miguel98@hotmail.com>
2020-11-23 14:08:59 +0100

    Añado funcion vercel [skip ci]

---
+++ api/consultaProducto.js
@@ -0,0 +1,10 @@
+const datos = require("./productos.json");
+const Dieta  = require("../src/dieta.js");
+const Productos = require("../src/producto.js");
+
+module.exports = (req,res) =>{
+
+    // Se envía el resultado
+    res.status(200).send(result)
+
+}
