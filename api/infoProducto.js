e8fd9a03682091e8aaf99882e8bdec4a5bbb3f49
josemip98 <j_miguel98@hotmail.com>
2020-11-24 16:44:10 +0100

    Añado funcion bot [skip ci]

---
+++ api/infoProducto.js
@@ -0,0 +1,55 @@
+const datos = require("./productos.json");
+const Dieta  = require("../src/dieta.js");
+const Producto = require("../src/producto.js");
+
+//AÑADIMOS FUNCION PARA LISTAR PRODUCTOS
+function listadoProductos(){
+
+    var i = 0;
+    var producto = new Producto();
+    while( i < datos.productos.length){
+      nombre= datos.productos[i]["nombre"];
+      calorias= datos.productos[i]["calorias"];
+      grasa= datos.productos[i]["grasa"];
+      proteinas= datos.productos[i]["proteinas"];
+      hidratos= datos.productos[i]["hidratos"];
+      i+=1
+      var dieta = new Dieta(nombre,sinopsis,num_temporadas,gen)
+      producto.aniadirSerie(serie)
+    }
+    var result = ""
+    //Tras ello captamos la series por genero
+    listadoProductos = producto.listadoProductos()
+    for(var j = 0; j < listadoProductos.length ; j++){
+        result = "Producto: " + nombre + ", calorias: " + calorias + ", grasa: " + grasa + ", hidratos: " + hidratos + ", proteinas: " + proteinas ;
+    }
+
+    return result
+}
+
+
+module.exports = async (req,res) =>{
+    //Que contiene req.body
+    if(req.body != undefined){
+        //Captamos id del mensaje
+        var IDchat = req.body.message.chat.id
+        //Captamos contenido de mensaje
+        var text = req.body.message.text
+        var result = ""
+        // Según el contenido obtenemos una lista de series u otra
+        if(text == "/lista"){
+            result="Listado de productos....\n";
+            result+=listadoProductos()
+        }
+
+        // Devolvemos
+        var objetoJSON ={text : result,method : "sendMessage",chat_id : IDchat}
+        res.setHeader("Content-Type","application/json");
+        res.status(200).json(objetoJSON)
+    }
+    else{
+        //Enviamos un mensaje normal
+        res.status(200).send("Iniciamos el bot de OrganizeUDiet")
+    }
+
+}
