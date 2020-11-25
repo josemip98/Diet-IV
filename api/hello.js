037efcf004ac2dc52449401a6094c149b6a65c06
josemip98 <j_miguel98@hotmail.com>
2020-11-25 21:40:36 +0100

    Añado funcion prueba vercel close #42 [skip ci]

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-    const { name = 'World' } = req.query
-    res.status(200).send(`Hello ${name}!`)
+    const { name = 'Mundo' } = req.query
+    res.status(200).send(`Hola ${name}!`)
   }
