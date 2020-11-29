5126757ce49a5d9b91e2f1d43da81d31c047568e
josemip98 <j_miguel98@hotmail.com>
2020-11-29 21:57:02 +0100

    Arreglo funcion vercel [skip ci]

--- api/consultaProducto.js
+++ api/consultaProducto.js
@@ -17,7 +17,7 @@ module.exports = (req,res) =>{
         var prod = new Producto(datos.productos[i].nombre,datos.productos[i].calorias,datos.productos[i].grasa,datos.productos[i].proteinas,datos.productos[i].hidratos);
         dieta.aniadirProducto(prod);
         //Si existe el producto lo muestra
-        if(dieta.mostrarProducto(prod) == producto){
+        if(dieta.mostrarProducto(prod.getNombre()) == producto){
           existeProducto = true;
 
           datosJSON.push({
