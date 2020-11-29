1a5aec05f068d2111e4e9e35ef7cd731182044bf
josemip98 <j_miguel98@hotmail.com>
2020-11-29 22:17:42 +0100

    Arreglo funcion vercel [skip ci]

--- api/consultaProducto.js
+++ api/consultaProducto.js
@@ -32,7 +32,7 @@ module.exports = (req,res) =>{
                   "Proteinas": producto_a_mostrar.getProteinas(),
                   "Hidratos": producto_a_mostrar.getHidratos()
           });
-            objeto[productos].push(result);
+            objeto.productos.push(result);
           //result = "Producto: " + nombre + ", calorias: " + calorias + ",
           // grasa: " + grasa + ", hidratos: " + hidratos + ", proteinas: "
           // + proteinas ;
