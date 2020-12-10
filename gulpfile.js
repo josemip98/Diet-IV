57915c919b5d6fce7e54f9f665013a890b1c1ca0
josemip98 <j_miguel98@hotmail.com>
2020-12-10 16:39:05 +0100

    Creo clase controlador [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
 var jest = require('gulp-jest').default;
+const pm2 = require('pm2');
 
 // Tarea para instalación de dependencias
 gulp.task('install', shell.task(['npm install']));
@@ -10,3 +11,21 @@ gulp.task('test', () => (
     gulp.src('tests', {read: false})
         .pipe(jest({reporter:'default'}))
 ));
+
+// Tarea para arrancar el microservicio
+gulp.task('start', function () {
+  pm2.connect(true, function () {
+    pm2.start({
+      name: 'OrganizeUDiet',
+      script: 'bin/www',
+      exec_mode: 'cluster',
+      instances: 4
+    }, function () {
+         console.log('Arrancando OrganizeUDiet.');
+         pm2.streamLogs('all', 0);
+       });
+  });
+});
+
+// Tarea para parar el microservicio
+gulp.task('stop', shell.task(['pm2 stop OrganizeUDiet']));
---
+++ src/controlador.js
@@ -0,0 +1,11 @@
+const Dieta = require("./dieta.js");
+const Producto = require("./producto.js");
+
+class Controlador {
+
+    constructor(){
+      this.dieta = new Dieta();
+    }
+
+
+}
