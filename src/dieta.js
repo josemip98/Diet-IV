864d61f3d2d002b570c5b7e7879928acc161f753
josemip98 <j_miguel98@hotmail.com>
2020-12-11 13:26:58 +0100

    Modifico funcion mostrarDieta clase dieta [skip ci]

--- src/dieta.js
+++ src/dieta.js
@@ -2,51 +2,9 @@ const Producto = require("../src/producto.js");
 
 class Dieta{
   //constructor
-  constructor(nombre_dieta, path_db){
-    this.path = path_db
-    this.fs = require('fs')
-
-         // Si no existe el archivo de integrantes, lo creamos inicianizándolo
-         if (!this.fs.existsSync(this.path)) {
-             var json = JSON.stringify(JSON.parse("[]"), null, 2);
-             this.fs.writeFileSync(this.path,json,'utf8',function(err){
-                 if(err) {
-                     throw err;
-                 }
-             });
-         }
-
-         // Si sí que existe el archivo simplemente lo abrimos y lo leemos.
-         else {
-             this.data = this.fs.readFileSync(this.path,'utf8',function(err){
-                 if(err) {
-                     throw err;
-                 }
-             });
-
-             // Si al leer el archivo, está vacío, lo inicializamos
-             if (this.data === ""){
-                 var json = JSON.stringify(JSON.parse("[]"), null, 2);
-                 this.fs.writeFileSync(this.path,json,'utf8',function(err){
-                     if(err) {
-                         throw err;
-                 }
-                 });
-             }
-
-             // Inicializamos los atributos de la clase en base a si ya hay información
-             // en el archivo de productos o no
-             var obj = JSON.parse(this.data);
-             // Comprobamos las parejas que ya se encuentran en el archivo y actualizamos
-             // las plazas disponibles
-         }
-
-         this.data = this.fs.readFileSync(this.path,'utf8',function(err){
-             if(err) {
-                 throw err;
-             }
-         });
-     }
+  constructor(){
+    this.listaProductos = new Array();
+  }
 
  //Función para añadir un producto a una dieta
   aniadirProducto(producto){
@@ -61,7 +19,7 @@ class Dieta{
   mostrarDieta(){
     var mostrarDieta = new Array();
     for(var i in this.listaProductos){
-      mostrarDieta.push(this.listaProductos[i].getProducto() + "\n ");
+      mostrarDieta.push(this.listaProductos[i]);
     }
     return mostrarDieta;
   }
